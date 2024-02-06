e9b6790439d148e0e86027c940e01b6ce05d65d2
Nikhi55 <nikhilburq70140@gmail.com>
2024-02-06 18:19:20 +0530

    working on utility

---
+++ e_commerce/lib/app.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'utils/theme/theme.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.system,
+      theme: TAppTheme.lightTheme,
+      darkTheme: TAppTheme.darkTheme,
+
+    );
+  }
+}
