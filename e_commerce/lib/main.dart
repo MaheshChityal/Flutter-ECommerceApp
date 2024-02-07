9d4f99d93df6993f207f92eb32f306fa5e6b3dad
Nikhi55 <chityalmahesh@gmail.com>
2024-02-07 12:59:36 +0530

    utility added

--- e_commerce/lib/main.dart
+++ e_commerce/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:e_commerce/utils/constants/colors.dart';
 import 'package:e_commerce/utils/theme/theme.dart';
 import 'package:flutter/material.dart';
 
@@ -7,6 +8,8 @@ void main() {
   // todo: Add Weidget Binding
   // todo: init Local Storage
   // todo: Await Native Splash
+
+ 
 
   runApp(const MyApp());
 }
