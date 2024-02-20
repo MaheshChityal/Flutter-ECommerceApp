4aa93f0988026e41633c96e492226a2c76bb99c5
Nikhi55 <chityalmahesh@gmail.com>
2024-02-20 18:13:18 +0530

    working on home page

--- e_commerce/lib/common/widgets/custom_shapes/container/curcular_container.dart
+++ e_commerce/lib/common/widgets/custom_shapes/container/curcular_container.dart
@@ -3,28 +3,35 @@ import 'package:flutter/material.dart';
 import '../../../../utils/constants/colors.dart';
 
 class CircularContainer extends StatelessWidget {
-  const CircularContainer( {
-    super.key, this.width = 400, this.height =400 , this.radius = 400, this.padding = 0, this.backgroundColor = TColors.white, this.child,
+  const CircularContainer({
+    super.key,
+    this.width = 400,
+    this.height = 400,
+    this.radius = 400,
+    this.padding = 0,
+    this.backgroundColor = TColors.white,
+    this.child,
+    this.margin,
   });
 
   final double? width;
   final double? height;
   final double radius;
   final double padding;
+  final EdgeInsets? margin;
   final Widget? child;
   final Color backgroundColor;
-
 
   @override
   Widget build(BuildContext context) {
     return Container(
       width: width,
       height: height,
+      margin: margin,
       padding: EdgeInsets.all(padding),
       decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(radius),
-          color: backgroundColor),
-          child: child,
+          borderRadius: BorderRadius.circular(radius), color: backgroundColor),
+      child: child,
     );
   }
 }
--- e_commerce/lib/features/shop/screens/home/home.dart
+++ e_commerce/lib/features/shop/screens/home/home.dart
@@ -1,8 +1,12 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:e_commerce/common/widgets/custom_shapes/container/curcular_container.dart';
+import 'package:e_commerce/utils/constants/image_strings.dart';
 import 'package:flutter/material.dart';
 import 'package:e_commerce/utils/constants/sizes.dart';
 
 import '../../../../common/widgets/custom_shapes/container/primary_header_container.dart';
 import '../../../../common/widgets/custom_shapes/container/search_container.dart';
+import '../../../../common/widgets/images/rounded_images.dart';
 import '../../../../common/widgets/texts/section_heading.dart';
 import '../../../../utils/constants/colors.dart';
 import 'widgets/home_appbar.dart';
@@ -13,7 +17,7 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SingleChildScrollView(
         child: Column(
           children: [
@@ -54,6 +58,39 @@ class HomeScreen extends StatelessWidget {
                         THomeCategories()
                       ],
                     ),
+                  )
+                ],
+              ),
+            ),
+
+            //Body Part
+
+            Padding(
+              padding: EdgeInsets.all(TSizes.defaultSpace),
+              child: Column(
+                children: [
+                  CarouselSlider(
+                    options: CarouselOptions(viewportFraction: 1),
+                    items: [
+                      TRoundedImage(imageUrl: TImages.promoBanner1),
+                      TRoundedImage(imageUrl: TImages.promoBanner2),
+                      TRoundedImage(imageUrl: TImages.promoBanner3),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: TSizes.spaceBtwItems,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      for(int i =0; i<3;i++)
+                      const CircularContainer(
+                        width: 20,
+                        height: 20,
+                        margin: EdgeInsets.only(right: 10),
+                        backgroundColor: TColors.black,
+                      ),
+                    ],
                   )
                 ],
               ),
